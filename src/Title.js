188d3610188beff306cacb7a5a0773a2a3593a74
MUDASSIRMunir <58942002+MUDASSIRMunir@users.noreply.github.com>
2020-06-15 14:29:38 +0500

    added title component

---
+++ src/Title.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+
+function Title() {
+  return (
+    <div className="App">
+     
+        <h1 style={ {color : "orange",fontFamily : "verdana"}}> BOOTCAMP SESSION 05 </h1>
+        <h1 style={ {color : "orange",fontFamily : "verdana"}}> Assignment 05 </h1>
+        
+    </div>
+  );
+}
+
+export default Title;
